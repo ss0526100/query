467d811b283ae7bf1f6a9976b0b2ea7e7e49fe15
Cha Seungha <ss0526100@gmail.com>
2025-05-02 13:59:31 +0900

    refactor (Vue Query): separate InitialDataType as a test utility

--- packages/vue-query/src/__tests__/test-utils.ts
+++ packages/vue-query/src/__tests__/test-utils.ts
@@ -50,3 +50,51 @@ export function successMutator<T>(param: T): Promise<T> {
 export function errorMutator<T>(_: T): Promise<Error> {
   return rejectFetcher()
 }
+
+// `initialData` can be `optional property` or `required property`.
+// These can be denoted with prefixes `p` and `r`, respectively.
+// Possible values are `undefined`, `()=>undefined`, `TData|undefined`, `()=>TData|undefined`, `TData`, `()=>TData`.
+// Each presence can be represented as a bit.
+// Combinations result in examples like:
+// ex)
+// {initialData ?: undefined} => p100000
+// {initialData : (()⇒TData|undefined) | TData} => r000110
+
+// For reference,
+// if the prefix is `r` && at least one of the last two bits is 1 && all bits except the last two are 0,
+// it should be `DefinedInitialQueryOptions`
+// otherwise, it should be `UndefinedInitialQueryOptions`.
+
+type Bit = '0' | '1'
+type OptionalPrefix = 'p' | 'r'
+type InitialDataCode = `${OptionalPrefix}${Bit}${Bit}${Bit}${Bit}${Bit}${Bit}`
+type StringTuple<TString extends string> =
+  TString extends `${infer Start}${infer Rest}`
+    ? [Start, ...StringTuple<Rest>]
+    : []
+
+type InitialDataType<TData, TTuple extends Array<string>> =
+  | (TTuple[1] extends '1' ? undefined : never)
+  | (TTuple[2] extends '1' ? () => undefined : never)
+  | (TTuple[3] extends '1' ? TData | undefined : never)
+  | (TTuple[4] extends '1' ? () => TData | undefined : never)
+  | (TTuple[5] extends '1' ? TData : never)
+  | (TTuple[6] extends '1' ? () => TData : never)
+
+type InitialDataOption<
+  TData,
+  TCode extends InitialDataCode,
+  TTuple extends Array<string> = StringTuple<TCode>,
+> = TTuple[0] extends 'p'
+  ? { initialData?: InitialDataType<TData, TTuple> }
+  : { initialData: InitialDataType<TData, TTuple> }
+
+export function getInitQueryOption<TData, TCode extends InitialDataCode>(
+  data: TData,
+) {
+  const defaultOption: { queryKey: ['key']; queryFn: () => TData } = {
+    queryKey: ['key'],
+    queryFn: () => data,
+  }
+  return defaultOption as typeof defaultOption & InitialDataOption<TData, TCode>
+}
